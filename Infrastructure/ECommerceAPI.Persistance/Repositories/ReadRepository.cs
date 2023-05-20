663999e5768ada691ab2b529bae3b810b866ab5d
Ibrahim ALTSOY <i.altsoy@gmail.com>
2023-05-20 13:42:26 -0700

    added tracking in repository

--- Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
+++ Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
@@ -17,21 +17,51 @@ namespace ECommerceAPI.Persistance.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-            => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+
+        }
 
 
-        public async Task<T> GetByIdAsync(string id)
-        //=>await Table.FirstOrDefaultAsync(x => x.Id == Guid.Parse(
-        // id));
-        => await Table.FindAsync(Guid.Parse(id));
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            //=>await Table.FirstOrDefaultAsync(x => x.Id == Guid.Parse(
+            // id));
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
-            => await Table.FirstOrDefaultAsync(method);
-       
+            // => await Table.FindAsync(Guid.Parse(id));
+        }
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-            =>Table.Where(method);
-        
+
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        { // => await Table.FirstOrDefaultAsync(method);
+            var query = Table.AsQueryable<T>();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+
+        }
+
+
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            // =>Table.Where(method);
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+            
+        }
+
+
     }
 }
--- Presentation/ECommerceAPI.Api/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.Api/Controllers/ProductsController.cs
@@ -8,33 +8,37 @@ namespace ECommerceAPI.Api.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductReadRepository productReadRepository;
-        private readonly IProductWriteRepository productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
 
         public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
         {
-            this.productReadRepository = productReadRepository;
-            this.productWriteRepository = productWriteRepository;
+            this._productReadRepository = productReadRepository;
+            this._productWriteRepository = productWriteRepository;
         }
 
 
         [HttpGet]
         public async Task Get()
         {
-            await productWriteRepository.CreateRangeAsync(new()
-            {
-                new(){Id = Guid.NewGuid(), Name = "Product-6", CreatedDate = DateTime.Now, Price = 100, Stock= 5},
-                new(){Id = Guid.NewGuid(), Name = "Product-7", CreatedDate = DateTime.Now, Price = 200, Stock= 15},
-                new(){Id = Guid.NewGuid(), Name = "Product-8", CreatedDate = DateTime.Now, Price = 300, Stock= 25},
-                new(){Id = Guid.NewGuid(), Name = "Product-9", CreatedDate = DateTime.Now, Price = 400, Stock= 35},
-                new(){Id = Guid.NewGuid(), Name = "Product-10", CreatedDate = DateTime.Now, Price = 500, Stock= 45},
-            });
-            await productWriteRepository.SaveAsync();
+            //await productWriteRepository.CreateRangeAsync(new()
+            //{
+            //    new(){Id = Guid.NewGuid(), Name = "Product-6", CreatedDate = DateTime.Now, Price = 100, Stock= 5},
+            //    new(){Id = Guid.NewGuid(), Name = "Product-7", CreatedDate = DateTime.Now, Price = 200, Stock= 15},
+            //    new(){Id = Guid.NewGuid(), Name = "Product-8", CreatedDate = DateTime.Now, Price = 300, Stock= 25},
+            //    new(){Id = Guid.NewGuid(), Name = "Product-9", CreatedDate = DateTime.Now, Price = 400, Stock= 35},
+            //    new(){Id = Guid.NewGuid(), Name = "Product-10", CreatedDate = DateTime.Now, Price = 500, Stock= 45},
+            //});
+            //await productWriteRepository.SaveAsync();
+            Product p = await _productReadRepository.GetByIdAsync("6550fe44-8cbc-4357-aca4-e27039120d50");
+           // Product p = await productReadRepository.GetByIdAsync("6550fe44-8cbc-4357-aca4-e27039120d50", false); yapacak olursak takibe alinmasin demis oluyoruz ve degisikligi yapmamis olur 
+            p.Name = "Monitor";
+            await _productWriteRepository.SaveAsync();
         }
         [HttpGet("{id}")]
          public async Task<IActionResult> GetById(string id)
         {
-            Product model = await productReadRepository.GetByIdAsync(id);
+            Product model = await _productReadRepository.GetByIdAsync(id);
             return Ok(model);
             
         }
