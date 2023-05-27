ab7699315877b585150d2842342b9131c09cc72a
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-05-26 21:23:12 -0700

    Cors

--- Presentation/ECommerceAPI.Api/Program.cs
+++ Presentation/ECommerceAPI.Api/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistanceServices();
-// Cors islemleri icin actigimiz alandir. Simdiki haliyle her istegi kabul eder diyoruz
+// Cors islemleri icin actigimiz alandir. Simdiki haliyle asagidan gelen adreslerden istegi kabul et dedik
 builder.Services.AddCors(options=> options.AddDefaultPolicy(policy=>
 policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200")));
 
